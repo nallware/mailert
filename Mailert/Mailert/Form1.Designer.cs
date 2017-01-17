dd38c2525d3c13f620d5423fff534f53c576ee69
KCCCURB\knall <knall@kcccurbs.com>
2017-01-17 11:01:24 -0500

    cleanup after including nuget package

--- Mailert/Mailert/Form1.Designer.cs
+++ Mailert/Mailert/Form1.Designer.cs
@@ -213,7 +213,6 @@
             this.Controls.Add(this.btnWait);
             this.Controls.Add(this.btnOK);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "GotMail";
