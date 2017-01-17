95b37ffb9642a89c6607ca3695995d78099a5177
Nallware <dev@nallware.com>
2017-01-17 10:00:54 -0500

    initial commit

---
+++ Mailert/Mailert/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace Mailert
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.checkTimer = new System.Windows.Forms.Timer(this.components);
+            this.btnOK = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.btnWait = new System.Windows.Forms.Button();
+            this.checkTimerLong = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbOkMins = new System.Windows.Forms.TextBox();
+            this.tbWaitMins = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSetup = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnOptions = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkTimer
+            // 
+            this.checkTimer.Interval = 30000;
+            this.checkTimer.Tick += new System.EventHandler(this.checkTimer_Tick);
+            // 
+            // btnOK
+            // 
+            this.btnOK.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnOK.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnOK.ForeColor = System.Drawing.Color.White;
+            this.btnOK.Location = new System.Drawing.Point(214, 104);
+            this.btnOK.Margin = new System.Windows.Forms.Padding(2);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(49, 36);
+            this.btnOK.TabIndex = 0;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = false;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Mailert Email Notifier";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.BalloonTipClicked += new System.EventHandler(this.notifyIcon1_BalloonTipClicked);
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            // 
+            // btnWait
+            // 
+            this.btnWait.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnWait.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnWait.ForeColor = System.Drawing.Color.White;
+            this.btnWait.Location = new System.Drawing.Point(267, 104);
+            this.btnWait.Margin = new System.Windows.Forms.Padding(2);
+            this.btnWait.Name = "btnWait";
+            this.btnWait.Size = new System.Drawing.Size(49, 36);
+            this.btnWait.TabIndex = 1;
+            this.btnWait.Text = "WAIT";
+            this.btnWait.UseVisualStyleBackColor = false;
+            this.btnWait.Click += new System.EventHandler(this.btnWait_Click);
+            // 
+            // checkTimerLong
+            // 
+            this.checkTimerLong.Interval = 300000;
+            this.checkTimerLong.Tick += new System.EventHandler(this.checkTimerLong_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Broadway", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(210, 33);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 21);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Email Notifier";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(5, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "New OK Mins";
+            // 
+            // tbOkMins
+            // 
+            this.tbOkMins.Location = new System.Drawing.Point(84, 190);
+            this.tbOkMins.Name = "tbOkMins";
+            this.tbOkMins.Size = new System.Drawing.Size(47, 20);
+            this.tbOkMins.TabIndex = 5;
+            this.tbOkMins.Text = "0.5";
+            // 
+            // tbWaitMins
+            // 
+            this.tbWaitMins.Location = new System.Drawing.Point(260, 190);
+            this.tbWaitMins.Name = "tbWaitMins";
+            this.tbWaitMins.Size = new System.Drawing.Size(47, 20);
+            this.tbWaitMins.TabIndex = 7;
+            this.tbWaitMins.Text = "5";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(173, 193);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "New Wait Mins";
+            // 
+            // btnSetup
+            // 
+            this.btnSetup.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnSetup.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSetup.ForeColor = System.Drawing.Color.White;
+            this.btnSetup.Location = new System.Drawing.Point(340, 190);
+            this.btnSetup.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSetup.Name = "btnSetup";
+            this.btnSetup.Size = new System.Drawing.Size(49, 21);
+            this.btnSetup.TabIndex = 8;
+            this.btnSetup.Text = "SET";
+            this.btnSetup.UseVisualStyleBackColor = false;
+            this.btnSetup.Click += new System.EventHandler(this.btnSetup_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = global::Mailert.Properties.Resources.wobot1;
+            this.pictureBox1.Image = global::Mailert.Properties.Resources.wobot1;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(180, 172);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnOptions
+            // 
+            this.btnOptions.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnOptions.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnOptions.ForeColor = System.Drawing.Color.White;
+            this.btnOptions.Image = global::Mailert.Properties.Resources.msgInfo15;
+            this.btnOptions.Location = new System.Drawing.Point(320, 104);
+            this.btnOptions.Margin = new System.Windows.Forms.Padding(2);
+            this.btnOptions.Name = "btnOptions";
+            this.btnOptions.Size = new System.Drawing.Size(49, 36);
+            this.btnOptions.TabIndex = 3;
+            this.btnOptions.UseVisualStyleBackColor = false;
+            this.btnOptions.Click += new System.EventHandler(this.btnOptions_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Broadway", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label4.Location = new System.Drawing.Point(210, 69);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 21);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Ver";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(400, 188);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnSetup);
+            this.Controls.Add(this.tbWaitMins);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbOkMins);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnOptions);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnWait);
+            this.Controls.Add(this.btnOK);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "GotMail";
+            this.TopMost = true;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer checkTimer;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Button btnWait;
+        private System.Windows.Forms.Timer checkTimerLong;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnOptions;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbOkMins;
+        private System.Windows.Forms.TextBox tbWaitMins;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSetup;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ Mailert/Mailert/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mailert
+{
+    public partial class Form1 : Form
+    {        
+        public Form1()
+        {
+            InitializeComponent();                        
+        }
+        public string version;
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            checkTimer.Enabled = true;
+            this.Hide();
+            notifyIcon1.BalloonTipText = "End Process.";
+            //notifyIcon1.BalloonTipTitle = "End";
+            version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            label4.Text = "Ver " + version;
+            notifyIcon1.Text = "Mailert Email Notifier" + Environment.NewLine + "Ver " + version;
+
+
+        }
+
+  
+
+        public bool RunCheck()
+        {
+            try
+            {
+                Microsoft.Office.Interop.Outlook.Application myApp = new Microsoft.Office.Interop.Outlook.Application();
+                Microsoft.Office.Interop.Outlook.NameSpace mapiNameSpace = myApp.GetNamespace("MAPI");
+                Microsoft.Office.Interop.Outlook.MAPIFolder myInbox = mapiNameSpace.GetDefaultFolder(Microsoft.Office.Interop.Outlook.OlDefaultFolders.olFolderInbox);
+
+                Microsoft.Office.Interop.Outlook.Items oItems = myInbox.Items.Restrict("[UnRead] = true");
+
+                if (oItems.Count > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch(Exception ex)
+            {
+                DialogResult dl = MessageBox.Show("A connection issue prevented Mailert from checking.  To shut down Mailert, select Abort.  Otherwise you can Retry now or Ignore this hiccup and let Mailert go back to monitoring.", "Warning", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Asterisk);
+                if (dl == DialogResult.Abort)
+                {
+                    Application.Exit();
+                }
+                else if (dl == DialogResult.Retry)
+                {
+
+                }
+                else
+                {
+                    this.Hide();
+                }
+                return false;
+            }
+        }
+
+        private void checkTimer_Tick(object sender, EventArgs e)
+        {            
+
+            if (RunCheck())
+            {
+                Screen screen = Screen.FromPoint(Cursor.Position);
+                this.Location = screen.Bounds.Location;                
+                this.Show();
+                this.CenterToScreen();
+            }
+            else
+            {
+                this.Hide();
+            }            
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.ExitThread();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            checkTimerLong.Enabled = false; 
+            checkTimer.Enabled = true;
+        }
+
+        private void notifyIcon1_BalloonTipClicked(object sender, EventArgs e)
+        {
+            
+            notifyIcon1.ShowBalloonTip(1);
+           
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            DialogResult dl = MessageBox.Show("End the Mailert Notifier?", "End Mailert Notifier", MessageBoxButtons.YesNo, MessageBoxIcon.Asterisk);
+            if (dl == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void btnWait_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            checkTimer.Enabled = false;
+            checkTimerLong.Enabled = true;
+        }
+
+        private void checkTimerLong_Tick(object sender, EventArgs e)
+        {           
+
+            if (RunCheck())
+            {
+                Screen screen = Screen.FromPoint(Cursor.Position);
+                this.Location = screen.Bounds.Location;
+                this.Show();
+                this.CenterToScreen();
+            }
+            else
+            {
+                this.Hide();
+            }
+        }
+
+        private void btnOptions_Click(object sender, EventArgs e)
+        {
+            this.Size = new Size(400, 222);
+            label2.Visible = true;
+            label3.Visible = true;
+            tbOkMins.Visible = true;
+            tbWaitMins.Visible = true;            
+        }
+
+        private void btnSetup_Click(object sender, EventArgs e)
+        {
+            
+
+            checkTimer.Interval = Convert.ToInt32(Convert.ToDouble(tbOkMins.Text)*1000*60);
+            checkTimerLong.Interval = Convert.ToInt32(Convert.ToDouble(tbWaitMins.Text)*1000*60);
+
+            this.Size = new Size(400, 188);
+            label2.Visible = false;
+            label3.Visible = false;
+            tbOkMins.Visible = false;
+            tbWaitMins.Visible = false;
+
+        }
+    }
+}
